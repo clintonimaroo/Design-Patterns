a2622f11bb51afb70426c155b9318f5af9010019
Name Can Not Be Blank <147147271+namecannotbeblankk@users.noreply.github.com>
2024-05-22 15:57:49 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Weervoorspelling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
